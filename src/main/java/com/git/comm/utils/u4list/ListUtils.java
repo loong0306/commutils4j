c6a9d964a0385cc72824c22d85060263f7729f42
dragon-yuan <plu_huajuan@163.com>
2017-09-16 22:01:04 +0800

    JAVA工具类收集整合commutils4j

--- src/main/java/com/git/comm/utils/u4list/ListUtils.java
+++ src/main/java/com/git/comm/utils/u4list/ListUtils.java
@@ -1,6 +1,8 @@
 package com.git.comm.utils.u4list;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class ListUtils {
 
@@ -11,5 +13,36 @@ public class ListUtils {
      */
     public static boolean isEmpty(Collection coll) {
         return coll == null || coll.isEmpty();
+    }
+
+    /**
+     * 拆分list，按500条拆分
+     * @param list
+     * @return
+     */
+    public static List<List<String>> splitList(List<String> list){
+        List<List<String>> lists = new ArrayList<List<String>>();
+        List<String> subList = new ArrayList<String>();
+        int size = list.size();
+        int sum = 500;
+        int count = size / sum;
+        int yu = size % sum;
+        if (count == 0) {
+            lists.add(list);
+        } else {
+            if(size % sum != 0){
+                count ++;
+            }
+            for (int i = 0; i < count; i++) {
+                if(sum*(i+1) <= size){
+                    subList = list.subList(sum*i, sum*(i+1));
+                }else{
+                    subList = list.subList(sum*i, sum*(i)+yu);
+                }
+                lists.add(subList);
+            }
+        }
+        return lists;
+
     }
 }
