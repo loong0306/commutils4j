91b206cc6bfb2b2a24e0c16784a02539b1def30c
dragon-yuan <plu_huajuan@163.com>
2017-09-24 22:08:28 +0800

    JAVA工具类收集整合commutils4j

--- src/main/java/com/git/comm/utils/u4double/DoubleUtils.java
+++ src/main/java/com/git/comm/utils/u4double/DoubleUtils.java
@@ -1,6 +1,7 @@
 package com.git.comm.utils.u4double;
 
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 public class DoubleUtils {
@@ -14,5 +15,53 @@ public class DoubleUtils {
         DecimalFormat df = new DecimalFormat();
         df.applyPattern("#0.00");
         return df.format(number);
+    }
+
+    /**
+     * 加法
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double add(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * 减法
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double sub(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * 乘法
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double mul(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * 除法
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double div(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.divide(b2, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 }
--- src/test/java/u4double/U4DoubleTest.java
+++ src/test/java/u4double/U4DoubleTest.java
@@ -10,6 +10,15 @@ public class U4DoubleTest {
 
     @Test
     public void test(){
+        // 将double类型的数字保留两位小数
         System.out.println(DoubleUtils.formatNumber(3.1415926));
+        // 加法
+        System.out.println(DoubleUtils.add(1.1,2.2));
+        // 减法
+        System.out.println(DoubleUtils.sub(5,0.9));
+        // 乘法
+        System.out.println(DoubleUtils.mul(3.3,0.1));
+        // 除法
+        System.out.println(DoubleUtils.div(5,0.5));
     }
 }
