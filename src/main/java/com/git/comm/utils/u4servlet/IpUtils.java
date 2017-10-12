a302fde9dbcf64a06b1576549e6f432af7e02f69
dragon-yuan <plu_huajuan@163.com>
2017-10-12 23:18:34 +0800

    JAVA工具类收集整合commutils4j

---
+++ src/main/java/com/git/comm/utils/u4servlet/IpUtils.java
@@ -0,0 +1,25 @@
+package com.git.comm.utils.u4servlet;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * ip工具类
+ */
+public final class IpUtils {
+
+    public static String getClientIp(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getRemoteAddr();
+        }
+        return ip.equals("0:0:0:0:0:0:0:1")?"127.0.0.1":ip;
+    }
+
+}
