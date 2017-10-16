fb429a38c3ef5693e223de8a5aea016b07f9163e
dragon-yuan <plu_huajuan@163.com>
2017-10-16 23:35:00 +0800

    JAVA工具类收集整合commutils4j

---
+++ src/main/java/com/git/comm/utils/u4list/FtpUtils.java
@@ -0,0 +1,75 @@
+package com.git.comm.utils.u4list;
+
+import org.apache.commons.vfs2.*;
+import org.apache.commons.vfs2.provider.ftp.FtpFileSystemConfigBuilder;
+import org.apache.commons.vfs2.provider.sftp.SftpFileSystemConfigBuilder;
+
+import java.io.IOException;
+
+/**
+ * FtpUtils
+ */
+public class FtpUtils {
+
+    /**
+     * 得到远程文件列表
+     */
+    public static FileObject[] getFilesByFtp(String url){
+        FileObject[] fileObjects = null;
+        try {
+            FileSystemManager fsManager = VFS.getManager();
+            FileObject fo = fsManager.resolveFile(url);
+            FileSystemOptions options = new FileSystemOptions();
+            FtpFileSystemConfigBuilder.getInstance().setUserDirIsRoot(options, false);
+            fileObjects = fo.getChildren();
+        } catch (FileSystemException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileObjects;
+    }
+
+    /**
+     * 筛选所需文件
+     */
+    public static FileObject[] getNeedFiles(String url){
+        FileObject[] fileObjects = getFilesByFtp(url);
+        FileObject[] newFileObjects = null;
+        for (int i = 0; i < fileObjects.length; i++){
+            // 在此处做文件筛选
+        }
+        return newFileObjects;
+    }
+
+    /**
+     * ftp/sftp
+     * 通过前置服务器无跳板机上传至所需服务器
+     * @param sourcePath 前置服务器地址
+     * @param targetPath 上传服务器地址
+     * @throws FileSystemException
+     */
+    public static void copyFile(String sourcePath, String targetPath) throws FileSystemException {
+        try {
+            FileObject[] getDownloadFiles = null;
+            // 获取需要上传的文件
+            FileSystemManager fsManager = VFS.getManager();
+            // 全部文件
+            getDownloadFiles = getFilesByFtp(sourcePath);
+            // 如需筛选，选用下面方法
+            // getDownloadFiles = getNeedFiles(sourcePath);
+            // 上传文件至服务器
+            FileSystemOptions opts = new FileSystemOptions();;
+            SftpFileSystemConfigBuilder.getInstance().setStrictHostKeyChecking(opts, "no");
+            FileObject getUploadFiles = fsManager.resolveFile(targetPath, opts);
+            for (int i = 0; i < getDownloadFiles.length; i++) {
+                FileObject tmp = fsManager.resolveFile(getUploadFiles, getDownloadFiles[i].getName().getBaseName());
+                if (!tmp.exists()) {
+                   tmp.copyFrom(getDownloadFiles[i], Selectors.SELECT_SELF);
+                }
+            }
+        } catch (FileSystemException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/u4os/U4FtpUtils.java
@@ -0,0 +1,27 @@
+package u4os;
+
+import com.git.comm.utils.u4list.FtpUtils;
+import org.junit.Test;
+
+/**
+ * Created by dragon on 10/16/2017.
+ */
+public class U4FtpUtils {
+
+    private final String downloadFtpName = "";
+    private final String downloadFtpPwd = "";
+    private final String downloadFtpUrl = "";
+    private final String downloadFtpFolder = "";
+
+    private final String uploadFtpName = "";
+    private final String uploadFtpPwd = "";
+    private final String uploadFtpUrl = "";
+    private final String uploadFtpFolder = "";
+
+    @Test
+    public void test() throws Exception {
+        String sourcePath = "ftp://" + downloadFtpName + ":" + downloadFtpPwd + "@" + downloadFtpUrl + downloadFtpFolder;
+        String targetPath = "ftp://" + uploadFtpName + ":" + uploadFtpPwd + "@" + uploadFtpUrl + uploadFtpFolder;
+        FtpUtils.copyFile(sourcePath, targetPath);
+    }
+}
